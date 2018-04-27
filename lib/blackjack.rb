6b2639a6c447a89167d091e502edd6deb8423f59
Roniece A Ricardo <roniecericardo@gmail.com>
2018-04-27 20:08:31 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -55,8 +55,8 @@ end
 
 def runner
  welcome
- initial_round
-   until display_card_total >= 21 do
+ card_total =initial_round
+   until display_card_total >= 21
     card_total = hit?(card_total)
     display_card_total(card_total)
    end
