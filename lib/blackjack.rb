0988834a3b4551a5caf6078b13e603d78154601a
Roniece A Ricardo <roniecericardo@gmail.com>
2018-04-27 20:09:20 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -8,7 +8,6 @@ end
 
 def display_card_total(cards)
   puts "Your cards add up to #{cards}"
-  cards
 end
 
 def prompt_user
@@ -55,7 +54,7 @@ end
 
 def runner
  welcome
- card_total =initial_round
+ card_total = initial_round
    until display_card_total > 21
     card_total = hit?(card_total)
     display_card_total(card_total)
